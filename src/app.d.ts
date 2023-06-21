9ba4c0e2a1ef8881e98950f833c5bd84a1a16cd2
yutabe <abe21453@cs.saisoncard.co.jp>
2023-06-21 16:41:26 +0900

    m

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,7 +4,9 @@ declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
+		interface PageData { 
+			user: SpotifyApi.CurrentProfileResponse | null;
+		}
 		// interface Platform {}
 	}
 }
